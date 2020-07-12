7715438d518e3b68103d06709ac782888dda6809
andrey <ndrydbv@gmail.com>
2020-07-12 20:39:28 +0300

    handle preventing duplicate cities, preventing invalid cities, and the ability to delete existing cities

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,5 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 
@@ -7,9 +7,32 @@ from .forms import CityForm
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=API_KEY'
 
+    err_msg = ''
+    message = ''
+    message_class = ''
+
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist in the world'
+            else:
+                err_msg = 'City already exists in the database.'
+
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City addes successfully'
+            message_class = 'is-success'
 
     form = CityForm()
 
@@ -27,6 +50,16 @@ def index(request):
         }
         weather_data.append(city_weather)
 
-    print(weather_data)
-    context = {'weather_data': weather_data, 'form': form}
+    context = {
+        'weather_data': weather_data,
+        'form': form,
+        'message': message,
+        'message_class': message_class
+    }
+
     return render(request, 'weather/weather.html', context)
+
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
